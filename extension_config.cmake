c1c991d0505d8409d380b44af12158ff5a081b10
dentinyhao <dentinyhao@gmail.com>
2026-09-22 14:59:37 -0700

    format repo

--- extension_config.cmake
+++ extension_config.cmake
@@ -1,14 +1,15 @@
 # Cargo package version is the source of truth for the extension metadata.
-file(STRINGS "${CMAKE_CURRENT_LIST_DIR}/rust/Cargo.toml" LANCE_PACKAGE_VERSION_LINE
-     REGEX "^version = \"[^\"]+\"$" LIMIT_COUNT 1)
-string(REGEX REPLACE "^version = \"([^\"]+)\"$" "\\1"
-       LANCE_CONVERSION_VERSION "${LANCE_PACKAGE_VERSION_LINE}")
+file(
+  STRINGS "${CMAKE_CURRENT_LIST_DIR}/rust/Cargo.toml" LANCE_PACKAGE_VERSION_LINE
+  REGEX "^version = \"[^\"]+\"$"
+  LIMIT_COUNT 1)
+string(REGEX REPLACE "^version = \"([^\"]+)\"$" "\\1" LANCE_CONVERSION_VERSION
+                     "${LANCE_PACKAGE_VERSION_LINE}")
 if("${LANCE_CONVERSION_VERSION}" STREQUAL "")
-  message(FATAL_ERROR "Cannot read lance-conversion package version from Cargo.toml")
+  message(
+    FATAL_ERROR "Cannot read lance-conversion package version from Cargo.toml")
 endif()
 
-duckdb_extension_load(lance_conversion
-    SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
-    EXTENSION_VERSION ${LANCE_CONVERSION_VERSION}
-)
+duckdb_extension_load(lance_conversion SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
+                      EXTENSION_VERSION ${LANCE_CONVERSION_VERSION})
 duckdb_extension_load(parquet)
